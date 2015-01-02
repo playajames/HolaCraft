04c69e92129f21e81d9e43262fb0ee378919e553
Kyle <kylegeraldedwards@gmail.com>
2015-01-02 00:52:48 +0000

    Chat while AFK removes you from AFK.

--- src/tk/holacraft/handlers/Chat.java
+++ src/tk/holacraft/handlers/Chat.java
@@ -1,5 +1,11 @@
 package tk.holacraft.handlers;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+
+import tk.holacraft.GlobalData;
 import tk.holacraft.Main;
 
 
@@ -9,5 +15,14 @@ public class Chat {
 	Main plugin;
 	public Chat(Main plugin) {
 		this.plugin = plugin;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void onChat(PlayerChatEvent e) {
+		Player player = e.getPlayer();
+		if(player.hasMetadata("afk")) {
+			player.setMetadata("afk", new FixedMetadataValue(plugin, 0));
+			player.sendMessage(GlobalData.styleChatServer + ChatColor.GRAY + "You are no-longer AFK.");
+		}
 	}
 }
