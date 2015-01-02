c712048b9efccce1b7e920d1f5931e9a39c6be9e
Kyle <kylegeraldedwards@gmail.com>
2015-01-02 01:12:41 +0000

    Switched classes for the AFK chat.

--- src/tk/holacraft/handlers/Chat.java
+++ src/tk/holacraft/handlers/Chat.java
@@ -1,13 +1,6 @@
 package tk.holacraft.handlers;
 
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerChatEvent;
-import org.bukkit.metadata.FixedMetadataValue;
-
-import tk.holacraft.GlobalData;
 import tk.holacraft.Main;
-
 
 public class Chat {
 
@@ -17,12 +10,5 @@ public class Chat {
 		this.plugin = plugin;
 	}
 	
-	@SuppressWarnings("deprecation")
-	public void onChat(PlayerChatEvent e) {
-		Player player = e.getPlayer();
-		if(player.hasMetadata("afk")) {
-			player.setMetadata("afk", new FixedMetadataValue(plugin, 0));
-			player.sendMessage(GlobalData.styleChatServer + ChatColor.GRAY + "You are no-longer AFK.");
-		}
-	}
+
 }
